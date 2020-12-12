228a9da72e619d8d0e13dc58430398094f8615f1
kanmadozo <trakyrichardkanm3@gmail.com>
2020-12-12 15:25:28 +0100

    Nairobi is now a json

--- src/Components/Dashboard/ChartGraph.js
+++ src/Components/Dashboard/ChartGraph.js
@@ -1,29 +1,94 @@
 import { Typography } from '@material-ui/core';
 import React, { Component } from 'react';
-import { Bar } from 'react-chartjs-2';
 import classes from './ChartGraph.module.css';
+import Chart from "chart.js";
+let myChart2;
 
 export default class ChartGraph extends Component {
+
+    chartRef2 = React.createRef();
+
+    componentDidMount() {
+        this.buildChart2();
+    }
+
+    componentDidUpdate() {
+        this.buildChart2();
+    }
+
     state = {
-        groupedBarsChartData: {
-            labels: ["Midnight-04 AM", "04 AM - 08 AM", "08 AM -12 PM", "12 PM - 04 PM", "04 PM - 8 PM", "08 PM - Midnight"],
-            datasets: [
-                {
-                    label: "Mombasa Road (sensor_1)",
-                    backgroundColor: "#3e95cd",
-                    data: [133, 221, 783, 2478, 133, 221,]
-                }, {
-                    label: "Nairobi_central (sensor_2)",
-                    backgroundColor: "#8e5ea2",
-                    data: [408, 547, 675, 734, 408, 547]
+        // groupedBarsChartData: {
+        //     labels: ["Midnight-04 AM", "04 AM - 08 AM", "08 AM -12 PM", "12 PM - 04 PM", "04 PM - 8 PM", "08 PM - Midnight"],
+        //     datasets: [
+        //         {
+        //             label: "Mombasa Road (sensor_1)",
+        //             backgroundColor: "#3e95cd",
+        //             data: [133, 221, 783, 2478, 133, 221,]
+        //         }, {
+        //             label: "Nairobi_central (sensor_2)",
+        //             backgroundColor: "#8e5ea2",
+        //             data: [408, 547, 675, 734, 408, 547]
+        //         },
+        //         {
+        //             label: "Test_sensor_2",
+        //             backgroundColor: "#8e5ea2",
+        //             data: [408, 547, 675, 734, 408, 547,]
+        //         }
+        //     ]
+        // }
+    }
+
+    buildChart2 = () => {
+        const myChartRef2 = this.chartRef2.current.getContext("2d");
+        const { data, average, test, labels } = this.props;
+
+        if (typeof myChart2 !== "undefined") myChart2.destroy();
+
+        myChart2 = new Chart(myChartRef2, {
+            type: "bar",
+            data: {
+                //Bring in data
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Mombasa Road (sensor_1)",
+                        data: data,
+                        backgroundColor: "#3e95cd",
+
+                    },
+                    {
+                        label: "Nairobi_central (sensor_2)",
+                        data: average,
+                        backgroundColor: "#3e95cd",
+                    },
+                    {
+                        label: "Test_sensor_2",
+                        data: test,
+                        backgroundColor: "#8e5ea2",
+                    },
+                ]
+            },
+            options: {
+                //Customize chart options
+                legend: {
+                    display: true
                 },
-                {
-                    label: "Test_sensor_2",
-                    backgroundColor: "#8e5ea2",
-                    data: [408, 547, 675, 734, 408, 547,]
-                }
-            ]
-        }
+                scales: {
+                    // yAxes: [{
+                    //     ticks: {
+                    //         max: this.props.maxY,
+                    //         min: 0,
+                    //         stepSize: 3
+                    //     }
+                    // }]
+                },
+                //    title: {
+                //     display: this.props.display,
+                //     text: this.props.title
+                //    }
+            }
+        });
+
     }
     render() {
         // const chartOptions = {
@@ -34,10 +99,16 @@ export default class ChartGraph extends Component {
         // }
 
         return (
-            <div className={classes.barContainer}>
-                <Typography>#421 Africa smart Counting</Typography>
-                <Typography>Average people during the day for the city of Nairobi</Typography>
-                <Bar data={this.state.groupedBarsChartData}
+            <div>
+                <Typography color="secondary">#421 Africa smart Counting</Typography>
+                <Typography color="primary">Average people during the day for the city of Nairobi</Typography>
+                <div className={classes.barContainer}>
+                    <canvas
+                        id="myChart2"
+                        ref={this.chartRef2}
+                    />
+                </div>
+                {/* <Bar data={this.state.groupedBarsChartData}
                     options={{
                         legend: {
                             display: true
@@ -49,7 +120,7 @@ export default class ChartGraph extends Component {
                         }
                     }}
                     width={350}
-                    height={250} />
+                    height={250} /> */}
             </div>
         )
     }
--- src/Views/Dashboard/Dashboard.js
+++ src/Views/Dashboard/Dashboard.js
@@ -4,7 +4,8 @@ import LineGraph from "../../Components/Dashboard/LineGraph";
 import chartIcon from "../../assets/chart-icon.svg";
 // import { managerData, nationalAverageData, yearLabels, managerQuarterData, nationalAverageQuarterData, quarterLabels } from "../../mockData";
 
-import all from '../../data/all.json'
+import all from '../../data/all.json';
+import Nairobi from '../../data/Nairobi.json'
 import ChartGraph from '../../Components/Dashboard/ChartGraph';
 
 export default class Dashboard extends Component {
@@ -14,10 +15,26 @@ export default class Dashboard extends Component {
         // labels: yearLabels,
         labels: [],
         persMoyenNbre: [],
-        PollutionNbre: []
+        PollutionNbre: [],
+        labelNairobi: [],
+        dataMombasa: [],
+        dataNairobiCentral: [],
+        dataTestSensor2: [],
+
     }
 
+
     componentDidMount() {
+
+        const getALlJson = async () => {
+            // await fetch("https://ces-iot-scan-wifi-devices.s3-eu-west-1.amazonaws.com/africa_smart_counting/konnectData.json").then((data) => {
+            //     console.log(data);
+            // })
+            // axios({
+            //     url: 'https://ces-iot-scan-wifi-devices.s3-eu-west-1.amazonaws.com/africa_smart_counting/konnectData.json',
+            //     method: 'get'
+            // }).then((response) => console.log(response))
+        }
         all.forEach((data) => {
             this.setState({
                 persMoyenNbre: data.persMoyenNbre,
@@ -25,7 +42,20 @@ export default class Dashboard extends Component {
                 labels: data.label
             })
         });
+
+        Nairobi.forEach((data) => {
+            this.setState({
+                labelNairobi: data.labelNairobi,
+                dataMombasa: data.dataMombasa,
+                dataNairobiCentral: data.dataNairobiCentral,
+                dataTestSensor2: data.dataTestSensor2,
+
+            })
+        })
+
+        getALlJson()
     }
+
 
     // handleButtonClick = e => {
     //     const { value } = e.target;
@@ -45,7 +75,7 @@ export default class Dashboard extends Component {
     // }
 
     render() {
-        const { labels, persMoyenNbre, PollutionNbre } = this.state;
+        const { labels, persMoyenNbre, PollutionNbre, dataMombasa, dataNairobiCentral, dataTestSensor2, labelNairobi } = this.state;
         return (
             <div className={classes.container}>
                 <header>
@@ -75,7 +105,7 @@ export default class Dashboard extends Component {
                     labels={labels} />
                 <br />
                 <br />
-                <ChartGraph
+                <ChartGraph data={dataMombasa} average={dataNairobiCentral} test={dataTestSensor2} labels={labelNairobi}
                 />
 
             </div>
